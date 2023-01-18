63ef42d863e5a18ff9e949facafa86f5db478a34
Leonamin <51023129+Leonamin@users.noreply.github.com>
2023-01-18 22:20:22 +0900

    추가 - 구글맵 테스트

--- lib/presentation/pages/create_destination/destination_setting_page.dart
+++ lib/presentation/pages/create_destination/destination_setting_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:you_are_finally_awake/di/location_service.dart';
 import 'package:you_are_finally_awake/di/service_locator.dart';
 
@@ -10,10 +13,22 @@ class DestinationSettingPage extends StatefulWidget {
 }
 
 class _DestinationSettingPageState extends State<DestinationSettingPage> {
+  Completer<GoogleMapController> _googleMapController = Completer();
+
+  final double _defaultBorderRadius = 24;
+
+  LatLng currentLatLng = const LatLng(45.521563, -122.677433);
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
   }
 
   void getLocation() {
@@ -24,11 +39,75 @@ class _DestinationSettingPageState extends State<DestinationSettingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: SingleChildScrollView(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Container(
-              color: Colors.amber,
+            ClipRRect(
+              borderRadius: BorderRadius.circular(_defaultBorderRadius),
+              child: SizedBox(
+                height: 300,
+                // SingleChildScrollView에 넣으면 화면 이동을 제대로 할 수 없다
+                child: GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                    target: currentLatLng,
+                    zoom: 11.0,
+                  ),
+                  onMapCreated: (controller) {
+                    _googleMapController.complete(controller);
+                    locator<LocationService>().getLocation().then(
+                      (value) {
+                        print("초기 값! = $value");
+
+                        if (value != null &&
+                            value.latitude != null &&
+                            value.longitude != null) {
+                          print("설정!");
+                          currentLatLng =
+                              LatLng(value.latitude!, value.longitude!);
+                          _googleMapController.future.then(
+                            (GoogleMapController controller) {
+                              // controller.moveCamera(
+                              //   CameraUpdate.newCameraPosition(
+                              //     CameraPosition(
+                              //         target: currentLatLng, zoom: 11.0),
+                              //   ),
+                              // );
+                              controller.animateCamera(
+                                CameraUpdate.newCameraPosition(
+                                  CameraPosition(
+                                      target: currentLatLng, zoom: 11.0),
+                                ),
+                              );
+                            },
+                          );
+                        }
+                      },
+                    );
+                  },
+                ),
+              ),
+            ),
+            Flexible(
+              child: ListView(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(_defaultBorderRadius),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 300,
+                          color: Colors.blue,
+                        ),
+                        Container(
+                          height: 300,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
